72399a20f700a01505e22ef2028ab75c331966cb
janitastic <palomino.jani@gmail.com>
2021-03-28 19:42:49 -0700

    Add functions

--- src/Library.js
+++ src/Library.js
@@ -7,7 +7,24 @@ function createLibrary(name) {
       nonFiction: [],
     },
   };
-};
+  return library;
+}
+
+function addBook(library, book) {
+  library['shelves'][book.genre].push(book)
+}
+
+function checkoutBook(library, bookTitle, genre) {
+  var userAlert = `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+    for (var i = 0; i < library["shelves"][genre].length; i++) {
+    //console.log(library["shelves"][genre][i]);
+    if (library["shelves"][genre][i]["title"] === bookTitle) {
+      library["shelves"][genre].pop();
+      userAlert = `You have now checked out ${bookTitle} from the ${library.name}` }
+   }
+   return userAlert;
+}
+
 
 module.exports = {
   createLibrary,
